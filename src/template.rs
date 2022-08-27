f974f04992b4d0537808524ea5eff85dfb2097ba
CaiHuajiang <huajiang.cai@fxexpert.cn>
2022-08-28 02:55:53 +0800

    支持动态拼装SQL语句

--- src/template.rs
+++ src/template.rs
@@ -1,4 +1,5 @@
 use std::{
+    cell::RefCell,
     collections::HashMap,
     sync::{
         atomic::{AtomicBool, Ordering},
@@ -7,24 +8,58 @@ use std::{
 };
 
 use bson::Bson;
-use handlebars::Handlebars;
+use handlebars::{Context, Handlebars, Helper, Output, RenderContext, RenderError};
 use lazy_static::lazy_static;
 use serde::Serialize;
 
-use crate::{ContextExt, Result, ERR_ARGUMENT, ERR_FORMAT};
+use crate::{BsonConvertExt, ContextExt, Result, ERR_ARGUMENT, ERR_FORMAT};
 
 lazy_static! {
     static ref GLOBAL_TEMPLATE: Arc<Mutex<Handlebars<'static>>> =
         Arc::new(Mutex::new(Handlebars::new()));
     static ref GLOBAL_TEMPLATE_INITED: AtomicBool = AtomicBool::new(false);
 }
+thread_local! (
+    static PLACE_CONTEXT: RefCell<HashMap<String,Bson>> = RefCell::new(HashMap::new())
+);
 
 fn get_handlebars() -> MutexGuard<'static, Handlebars<'static>> {
-    let global = GLOBAL_TEMPLATE.lock().unwrap();
+    let mut global = GLOBAL_TEMPLATE.lock().unwrap();
     if !GLOBAL_TEMPLATE_INITED.load(Ordering::Relaxed) {
+        init(&mut global);
         GLOBAL_TEMPLATE_INITED.store(true, Ordering::Relaxed);
     }
     global
+}
+
+/// 模板引擎初始化
+fn init(global: &mut MutexGuard<Handlebars>) {
+    global.register_helper("$", Box::new(place_helper));
+}
+
+/// 生成占位符，可用于SQL但不具限于SQL拼装场景
+fn place_helper(
+    h: &Helper,
+    _hb: &Handlebars,
+    _c: &Context,
+    _rc: &mut RenderContext,
+    out: &mut dyn Output,
+) -> std::result::Result<(), RenderError> {
+    PLACE_CONTEXT.with(|ctx| {
+        let mut map = ctx.borrow_mut();
+        let value = h.param(0).ok_or(RenderError::new("参数不能为空.")).unwrap();
+        let name = h.param(1);
+        let key;
+        if name.is_some() {
+            key = format!("{}", name.unwrap().render());
+        } else {
+            let pos = map.len();
+            key = format!("${}", pos);
+        }
+        out.write(key.as_str()).unwrap();
+        map.insert(key, value.value().as_bson());
+    });
+    Ok(())
 }
 
 /// 根据内容文本渲染模板
@@ -38,6 +73,33 @@ where
             .msg_detail("模板渲染失败".to_string())
             .cause(anyhow::Error::new(e))),
     }
+}
+
+/// 根据内容文本渲染模板
+pub fn render_template_with_place<C>(
+    template: String,
+    param: &C,
+) -> Result<(String, HashMap<String, Bson>)>
+where
+    C: Serialize,
+{
+    let mut map = HashMap::<String, ContextType>::new();
+    let key = "$template";
+    let mut attrs = vec![];
+    let param_obj = bson::to_bson(param).unwrap();
+    match param_obj {
+        Bson::Document(doc) => {
+            for (k, v) in doc {
+                map.insert(k.to_string(), ContextType::ValueType(v));
+                attrs.push(k.to_string());
+            }
+        }
+        v => {
+            map.insert("_root".to_string(), ContextType::ValueType(v));
+        }
+    }
+    map.insert_template(key, template.as_str(), attrs);
+    render_template_recursion(&map, key)
 }
 
 /// 上下文数据类型
@@ -63,11 +125,12 @@ impl ContextType {
     }
 }
 
-/// 根据模板递归调用子模板、计算类型及上下文进行渲染
+/// 根据模板递归调用子模板、计算类型及上下文进行渲染，支持返回占用类型的参数
 pub fn render_template_recursion(
     context: &HashMap<String, ContextType>,
     key: &str,
-) -> Result<String> {
+) -> Result<(String, HashMap<String, Bson>)> {
+    PLACE_CONTEXT.with(|ctx| ctx.borrow_mut().clear());
     let (root_template, root_attrs) = match context.get(&key.to_string()) {
         Some(v) => match v {
             ContextType::TemplateType { template, attrs } => (template.clone(), attrs),
@@ -75,43 +138,55 @@ pub fn render_template_recursion(
         },
         None => return Err(ERR_ARGUMENT.msg_detail(format!("模板定义{}不存在", &key))),
     };
-    if root_attrs.is_empty() {
-        return Ok(root_template.to_string());
-    }
     let mut param = HashMap::<String, Bson>::new();
-    for item_name in root_attrs {
-        match context.get(item_name) {
-            Some(child_v) => match child_v {
-                ContextType::TemplateType {
-                    template: _,
-                    attrs: _,
-                } => {
-                    param.insert_string(
-                        item_name.as_str(),
-                        render_template_recursion(context, item_name).unwrap(),
-                    );
-                }
-                ContextType::ValueType(v) => {
-                    param.insert(item_name.to_string(), v.clone());
-                }
-                ContextType::InvokerType(it) => {
-                    let context = unsafe {
-                        &mut *(context as *const HashMap<String, ContextType>
-                            as *mut HashMap<String, ContextType>)
-                    };
-                    param.insert(item_name.to_string(), it.as_ref()(context));
-                }
-            },
-            None => return Err(ERR_ARGUMENT.msg_detail(format!("模板定义{}不存在", item_name))),
-        };
+    if !root_attrs.is_empty() {
+        for item_name in root_attrs {
+            match context.get(item_name) {
+                Some(child_v) => match child_v {
+                    ContextType::TemplateType {
+                        template: _,
+                        attrs: _,
+                    } => {
+                        param.insert_string(
+                            item_name.as_str(),
+                            render_template_recursion(context, item_name).unwrap().0,
+                        );
+                    }
+                    ContextType::ValueType(v) => {
+                        param.insert(item_name.to_string(), v.clone());
+                    }
+                    ContextType::InvokerType(it) => {
+                        let context_mut = unsafe {
+                            &mut *(context as *const HashMap<String, ContextType>
+                                as *mut HashMap<String, ContextType>)
+                        };
+                        param.insert(item_name.to_string(), it.as_ref()(context_mut));
+                    }
+                },
+                None => return Err(ERR_ARGUMENT.msg_detail(format!("模板定义{}不存在", item_name))),
+            };
+        }
     }
-    render_template(root_template, &param)
+    let res;
+    if context.contains_key("_root") {
+        let context_mut = unsafe {
+            &mut *(context as *const HashMap<String, ContextType>
+                as *mut HashMap<String, ContextType>)
+        };
+        res = render_template(
+            root_template,
+            context_mut.get_bson("_root").as_ref().unwrap(),
+        );
+    } else {
+        res = render_template(root_template, &param);
+    }
+    res.map(|x| (x, PLACE_CONTEXT.with(|ctx| ctx.take())))
 }
 
 /// 键为字符类型且值为ContextType的Map工具操作类，主要用于模板生成
 pub trait TemplateContextExt {
     /// 插入模板类型
-    fn insert_template(&mut self, key: &str, template: &str, attrs: Vec<&str>);
+    fn insert_template(&mut self, key: &str, template: &str, attrs: Vec<String>);
     /// 插入可调用类型
     fn insert_invoker(
         &mut self,
@@ -130,7 +205,7 @@ impl ContextExt for HashMap<String, ContextType> {
     }
 }
 impl TemplateContextExt for HashMap<String, ContextType> {
-    fn insert_template(&mut self, key: &str, template: &str, attrs: Vec<&str>) {
+    fn insert_template(&mut self, key: &str, template: &str, attrs: Vec<String>) {
         self.insert(
             key.to_string(),
             ContextType::TemplateType {
@@ -150,3 +225,113 @@ impl TemplateContextExt for HashMap<String, ContextType> {
 }
 /// 可代替HashMap<String, ContextType>操作的工具
 pub type TemplateContext = HashMap<String, ContextType>;
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use bson::bson;
+
+    use crate::{
+        template::{render_template_recursion, render_template_with_place},
+        ContextExt, TemplateContextExt,
+    };
+
+    #[test]
+    fn test_render_template_recursion() {
+        let mut map = HashMap::new();
+        map.insert_template(
+            "sql",
+            r#"
+                select * from table where name={{$ data.name}} and address in 
+                {{#each data.address}}
+                    {{$ city}},
+                {{/each}}
+            "#,
+            vec!["data".to_string()],
+        );
+        map.insert_bson(
+            "data",
+            bson!({
+                "name": "zhangshan",
+                "age": [13, 14],
+                "address": [{
+                    "country": "china",
+                    "city": "shanghai",
+                },{
+                    "country": "usa",
+                    "city": "newyork",
+                }]
+            }),
+        );
+        let res = render_template_recursion(&map, "sql").unwrap();
+        println!("{:?}", res.0);
+        println!("{:?}", res.1);
+        assert!(res.0.contains(""));
+        assert!(res.1.contains_key(""));
+    }
+
+    #[test]
+    fn test_render_template_with_place1() {
+        let res = render_template_with_place(
+            r#"
+                select * from table where name={{$ this}} 
+            "#
+            .to_string(),
+            &"张三",
+        )
+        .unwrap();
+        println!("{:?}", res.0);
+        println!("{:?}", res.1);
+        assert!(res.0.contains(""));
+        assert!(res.1.contains_key(""));
+    }
+
+    #[test]
+    fn test_render_template_with_place2() {
+        let res = render_template_with_place(
+            r#"
+                select * from table where name in 
+                {{#each this}}
+                    {{$ this}},
+                {{/each}}
+            "#
+            .to_string(),
+            &["张三", "李四"],
+        )
+        .unwrap();
+        println!("{:?}", res.0);
+        println!("{:?}", res.1);
+        assert!(res.0.contains(""));
+        assert!(res.1.contains_key(""));
+    }
+
+    #[test]
+    fn test_render_template_with_place3() {
+        let res = render_template_with_place(
+            r#"
+                select * from table where name={{$ name}} and address in 
+                {{#each address}}
+                    {{$ city}},
+                {{/each}}
+            "#
+            .to_string(),
+            &bson!({
+                "name": "zhangshan",
+                "age": [13, 14],
+                "address": [{
+                    "country": "china",
+                    "city": "shanghai",
+                },{
+                    "country": "usa",
+                    "city": "newyork",
+                }]
+            }),
+        )
+        .unwrap();
+        println!("{:?}", res.0);
+        println!("{:?}", res.1);
+        assert!(res.0.contains("$0"));
+        assert!(res.1.contains_key("$0"));
+    }
+}
